e8f242640b57d2cf69fa768ccc9b2b9ab8908b76
Maria Valero <79057955+mvgame74@users.noreply.github.com>
2021-09-08 17:25:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import telebot
+
+API_KEY = os.getenv("API_KEY")
+bot = telebot.TeleBot(API_KEY)
+
+@bot.message_handler(commands=['Greet'])
+def greet(message):
+  bot.reply_to(message, "Hey! How is it going?")
+
+@bot.message_handler(commands=['Hello'])
+def hello(message):
+  bot.send_message(message.chat.id, "Hello to you too!")
+
+bot.polling()
